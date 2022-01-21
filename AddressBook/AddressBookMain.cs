4d5160a4d9b9ee561b113cdbe9137743d3d893b2
srikantheddula <you@example.com>
2022-01-21 20:53:01 +0530

    [srikantheddula]:add AddressBook Program

---
+++ AddressBook/AddressBookMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+     class AddressBookMain
+    {
+        /// <summary>
+        /// Addresses the book.
+        /// </summary>
+        public static void AddressBook() //creating Static Method
+        {
+            Console.WriteLine("Welcome to the AddressBook Program ");
+        }
+    }
+}
